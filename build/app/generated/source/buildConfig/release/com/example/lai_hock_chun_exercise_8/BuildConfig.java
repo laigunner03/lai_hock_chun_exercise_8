edc87925114a66de5b62edd7422b47fb84bb8458
Lai Hock Chun <jasonlai29@gmail.com>
2021-11-03 15:32:06 +0800

    Apk bundle

---
+++ build/app/generated/source/buildConfig/release/com/example/lai_hock_chun_exercise_8/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.lai_hock_chun_exercise_8;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.lai_hock_chun_exercise_8";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.1";
+}
